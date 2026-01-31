023061e61dbf172383d1563236d1f18ca00edec1
zekwxxx <144808634+zekwxxx@users.noreply.github.com>
2026-01-31 11:37:01 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const heart = document.getElementById("heart");
-const mainScreen = document.getElementById("mainScreen");
-const giftScreen = document.getElementById("giftScreen");
-
-heart.addEventListener("click", () => {
-  mainScreen.classList.remove("active");
-  giftScreen.classList.add("active");
-});
